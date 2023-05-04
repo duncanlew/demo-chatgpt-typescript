9a6aac100128f6f2d101952f353d918bd4916672
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-04 21:38:14 +0200

    get models

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Configuration, OpenAIApi } from 'openai';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listModels();
+
+console.log(response.data);
