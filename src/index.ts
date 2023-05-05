35be6d3f8acf4d482aaee3749217414099124e41
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-05 22:14:26 +0200

    soort van werkend

--- src/index.ts
+++ src/index.ts
@@ -14,39 +14,40 @@ const completion = await openai.createChatCompletion({
   messages: [{ role: "user", content: "Hello what are you doing?" }],
 });
 console.log(completion.data.choices[0].message);
+console.log("*****************************\nOne part of the data");
 console.log(completion.data);
-console.log("*****************************\nSomething else");
+console.log("*****************************\nWhole completion");
 console.log(completion);
 // *******************************************************
 
-const messages: string[] = [];
+// const messages: string[] = [];
 
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const userInterface = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-userInterface.setPrompt("Send a message:\n");
-userInterface.prompt();
+// userInterface.setPrompt("Send a message:\n");
+// userInterface.prompt();
 
-userInterface
-  .on("line", async (input) => {
-    const message: ChatCompletionRequestMessage = {
-      role: "user",
-      content: input,
-    };
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [message],
-    });
+// userInterface
+//   .on("line", async (input) => {
+//     const message: ChatCompletionRequestMessage = {
+//       role: "user",
+//       content: input,
+//     };
+//     const completion = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       messages: [message],
+//     });
 
-    console.log("You entered >> ", input);
-    messages.push(input);
-    console.log("Current messages:");
-    console.log(messages);
-    console.log("*************************");
-    userInterface.prompt();
-  })
-  .on("close", () => {
-    console.log("Thank you for using this Demo");
-  });
+//     console.log("You entered >> ", input);
+//     messages.push(input);
+//     console.log("Current messages:");
+//     console.log(messages);
+//     console.log("*************************");
+//     userInterface.prompt();
+//   })
+//   .on("close", () => {
+//     console.log("Thank you for using this Demo");
+//   });
