707a9fb77a7903e23b48361053ea8be70fe47f5c
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-05 22:31:43 +0200

    working

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,11 @@ import * as readline from "readline";
 import * as dotenv from "dotenv";
 import chalk from "chalk";
 
-// *******************************************************
 dotenv.config();
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
-// const completion = await openai.createChatCompletion({
-//   model: "gpt-3.5-turbo",
-//   messages: [{ role: "user", content: "Hello what are you doing?" }],
-// });
-// console.log(completion.data.choices[0].message);
-// console.log("*****************************\nOne part of the data");
-// console.log(completion.data);
-// console.log("*****************************\nWhole completion");
-// console.log(completion);
-// *******************************************************
 
 const messages: ChatCompletionRequestMessage[] = [];
 
@@ -52,11 +40,6 @@ userInterface
         content: responseMessage.content,
       });
     }
-
-    // console.log("You entered >> ", input);
-    // console.log("Current messages:");
-    // console.log(messages);
-    // console.log("*************************");
     userInterface.prompt();
   })
   .on("close", () => {
