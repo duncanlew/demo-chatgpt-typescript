2d8de905b76b9dc8902ec14391fc29ed15f0d50d
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-05 21:53:08 +0200

    rudimentary version

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,15 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listModels();
+// const response = await openai.listModels();
 
-console.log(response.data);
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{ role: "user", content: "Hello what are you doing?" }],
+});
+console.log(completion.data.choices[0].message);
+
+
+console.log(completion.data);
+console.log('*****************************\nSomething else');
+console.log(completion);
