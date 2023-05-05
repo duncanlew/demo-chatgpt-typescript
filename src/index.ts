3406ad16066eef1d3411891b057d3f4595e5e434
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-05 21:56:57 +0200

    readline stuff

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import { Configuration, OpenAIApi } from 'openai';
+import * as readline from 'readline';
 import * as dotenv from "dotenv";
+// const readline = require("readline");
 
-dotenv.config();
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+// *******************************************************
+// dotenv.config();
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const completion = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo",
+  //   messages: [{ role: "user", content: "Hello what are you doing?" }],
+  // });
+  // console.log(completion.data.choices[0].message);
+  // console.log(completion.data);
+  // console.log('*****************************\nSomething else');
+  // console.log(completion);
+  // *******************************************************
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-const openai = new OpenAIApi(configuration);
-// const response = await openai.listModels();
-
-const completion = await openai.createChatCompletion({
-  model: "gpt-3.5-turbo",
-  messages: [{ role: "user", content: "Hello what are you doing?" }],
+const messages: string[] = [];
+userInterface.prompt();
+userInterface.on("line", async (input) => {
+  console.log('You entered >> ', input);
+  messages.push(input);
+  console.log('Current messages:');
+  console.log(messages);
+  console.log('*************************');
+  userInterface.prompt();
 });
-console.log(completion.data.choices[0].message);
-
-
-console.log(completion.data);
-console.log('*****************************\nSomething else');
-console.log(completion);
