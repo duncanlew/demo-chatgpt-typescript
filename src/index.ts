2f0173cfcb92e019514b46daf87915ee7cf45514
Duncan Lew <duncanlew@users.noreply.github.com>
2023-05-05 22:12:56 +0200

    something

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,52 @@
-import { Configuration, OpenAIApi } from 'openai';
-import * as readline from 'readline';
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import * as readline from "readline";
 import * as dotenv from "dotenv";
-// const readline = require("readline");
 
 // *******************************************************
-// dotenv.config();
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
+dotenv.config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-// const completion = await openai.createChatCompletion({
-  //   model: "gpt-3.5-turbo",
-  //   messages: [{ role: "user", content: "Hello what are you doing?" }],
-  // });
-  // console.log(completion.data.choices[0].message);
-  // console.log(completion.data);
-  // console.log('*****************************\nSomething else');
-  // console.log(completion);
-  // *******************************************************
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{ role: "user", content: "Hello what are you doing?" }],
+});
+console.log(completion.data.choices[0].message);
+console.log(completion.data);
+console.log("*****************************\nSomething else");
+console.log(completion);
+// *******************************************************
+
+const messages: string[] = [];
 
 const userInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const messages: string[] = [];
+
+userInterface.setPrompt("Send a message:\n");
 userInterface.prompt();
-userInterface.on("line", async (input) => {
-  console.log('You entered >> ', input);
-  messages.push(input);
-  console.log('Current messages:');
-  console.log(messages);
-  console.log('*************************');
-  userInterface.prompt();
-});
+
+userInterface
+  .on("line", async (input) => {
+    const message: ChatCompletionRequestMessage = {
+      role: "user",
+      content: input,
+    };
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [message],
+    });
+
+    console.log("You entered >> ", input);
+    messages.push(input);
+    console.log("Current messages:");
+    console.log(messages);
+    console.log("*************************");
+    userInterface.prompt();
+  })
+  .on("close", () => {
+    console.log("Thank you for using this Demo");
+  });
